a8ae63eae6c13d50dbbca005d0560c074133c65f
Connor <connor.schenk12@gmail.com>
2025-08-20 12:59:00 -0600

    Create players.js

---
+++ src/data/players.js
@@ -0,0 +1,20 @@
+// super small starter pool; we’ll swap to a real NFL API later
+export const PLAYERS = [
+  { id: "qb_mahomes", name: "Patrick Mahomes", pos: "QB" },
+  { id: "qb_allen",   name: "Josh Allen",      pos: "QB" },
+
+  { id: "rb_mccaffrey", name: "Christian McCaffrey", pos: "RB" },
+  { id: "rb_bijan",      name: "Bijan Robinson",     pos: "RB" },
+
+  { id: "wr_jefferson",  name: "Justin Jefferson", pos: "WR" },
+  { id: "wr_chase",      name: "Ja'Marr Chase",   pos: "WR" },
+
+  { id: "te_kelce", name: "Travis Kelce", pos: "TE" },
+  { id: "te_sam",   name: "Sam LaPorta",  pos: "TE" },
+
+  { id: "k_butker", name: "Harrison Butker", pos: "K" },
+  { id: "k_tucker", name: "Justin Tucker",   pos: "K" },
+
+  { id: "def_49ers", name: "49ers DEF", pos: "DEF" },
+  { id: "def_ravens", name: "Ravens DEF", pos: "DEF" },
+];
